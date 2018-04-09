4bc2c17a515f7673a5df70eca44ffcacc021e489
asm-jaime <asm.jaime@gmail.com>
2018-04-09 11:09:56 +1000

    fix include dmctype

--- src/dmctype.h
+++ src/dmctype.h
@@ -1,22 +1,15 @@
-/* =================================================================================
-File name:        DMCTYPE.H                    
-===================================================================================*/
- 
-#ifndef DMCTYPE
-#define DMCTYPE
+#ifndef __DMCTYPE_H__
+#define __DMCTYPE_H__
 
-//---------------------------------------------------------------------------
 // For Portability, User Is Recommended To Use Following Data Type Size
 // Definitions For 16-bit and 32-Bit Signed/Unsigned Integers:
-//
 
-typedef int             int16;
-typedef long            int32;
-typedef unsigned int    Uint16;
-typedef unsigned long   Uint32;
-typedef float           float32;
-typedef long double     float64;
+typedef int           int16;
+typedef long          int32;
+typedef unsigned int  Uint16;
+typedef unsigned long Uint32;
+typedef float         float32;
+typedef long double   float64;
 
-
-#endif  // DMCTYPE
+#endif  // __DMCTYPE_H__
 
